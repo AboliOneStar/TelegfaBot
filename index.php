3c38e3cc00caa4711064c1d556212c6a60cfea7b
HaDi Agah <38810979+HazNet@users.noreply.github.com>
2019-01-13 18:26:46 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
 // Hi F**king World :D
+
+for($i=1; $i <= 8.5; $i+=0.5){
+$str = str_repeat('🍆🍑', $i);
+echo "$str".str_replace('.','',$i)." % <br>";
+}
+echo 'Coming Soon.';
+// this not empty project😐
